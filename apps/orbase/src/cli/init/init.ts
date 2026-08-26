fa878d66f02a292e3d4e77f7134e57578d9c3345
nazozokc <nazozokc@users.noreply.github.com>
2026-08-26 13:17:17 +0900

    edit

--- apps/orbase/src/cli/init/init.ts
+++ apps/orbase/src/cli/init/init.ts
@@ -6,11 +6,25 @@ import { join } from "node:path";
 
 export const init = async (): Promise<void> => {
   const choices = [];
-  const basedir = join(`${INIT_DIR}`, init);
   const currentDir = process.cwd();
-  const readed = await readdir(basedir);
+  const readed = await readdir(INIT_DIR);
 
   for (const file of readed) {
-    await checkbox({});
+    choices.push(file);
+  }
+
+  const selected = await checkbox({
+    message: "init files",
+    choices,
+  });
+
+  for (const select of selected) {
+    const read = join(`${INIT_DIR}`, select);
+    const files = await readdir(read);
+    for (const file of files) {
+      const source = join(read, file);
+      const toinit = join(currentDir, file);
+      await cp(source, toinit, { recursive: true });
+    }
   }
 };
