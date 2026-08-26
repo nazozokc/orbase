9f46893d26e2ca6c1604a9ee309fc82f24b5882e
nazozokc <nazozokc@users.noreply.github.com>
2026-08-26 13:59:26 +0900

    edit

--- apps/orbase/src/note/deletenote.ts
+++ apps/orbase/src/note/deletenote.ts
@@ -3,5 +3,5 @@ import { join } from "path";
 import { NOTE_DIR } from "../constant/app.js";
 
 export const deleteNote = async (name: string): Promise<void> => {
-  await unlink(join(NOTE_DIR, `${name}.md`));
+  await unlink(join(NOTE_DIR, `${name}`));
 };
