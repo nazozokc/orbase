289b277355dfb98d56ee93579309ee2f3991a10b
nazozokc <nazozokc@gmail.com>
2026-08-30 23:37:56 +0900

    edit

--- apps/orbase/src/note/book/bookSave.ts
+++ apps/orbase/src/note/book/bookSave.ts
@@ -1,11 +1,11 @@
 import consola from "consola";
-import { ROOT_DIR } from "../../constant/app";
+import { NOTE_DIR, ROOT_DIR } from "../../constant/app";
 import { mkdir, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 
 export const bookSave = async (filename: string): Promise<void> => {
   try {
-    const path = join(ROOT_DIR, filename);
+    const path = join(NOTE_DIR, filename);
     await mkdir(path, { recursive: true });
     const bookJson = JSON.stringify(filename, null, 2);
     await writeFile(`${ROOT_DIR}/book.json`, bookJson, "utf-8");
