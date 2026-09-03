df8db88ee460de7f650906d35e3e30458f3fd53a
nazozokc <nazozokc@gmail.com>
2026-09-03 15:44:13 +0900

    edit

--- apps/orbase/src/task/writeTask.ts
+++ apps/orbase/src/task/writeTask.ts
@@ -1,10 +1,18 @@
 import { writeFile, mkdir } from "node:fs/promises";
-import { type Task } from "./type.ts";
+import { z } from "zod";
+import { TaskSchema, type Task } from "./type.ts";
 import { TASK_DIR } from "../constant/app.ts";
 import consola from "consola";
 
 export const writeTask = async (task: Task): Promise<void> => {
   try {
+    const result = TaskSchema.safeParse(task);
+
+    if (!result.success) {
+      consola.error(result.error);
+      return;
+    }
+
     await mkdir(TASK_DIR, { recursive: true });
     const taskJson = JSON.stringify(task, null, 2);
     await writeFile(`${TASK_DIR}/${task.id}.json`, taskJson, "utf-8");
