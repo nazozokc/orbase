ba64718a8b704ba415cdb1010be0ed00359ed491
nazozokc <nazozokc@users.noreply.github.com>
2026-09-06 23:31:14 +0900

    edit

--- apps/orbase/src/tags/tagSave.ts
+++ apps/orbase/src/tags/tagSave.ts
@@ -4,26 +4,25 @@ import consola from "consola";
 import { z } from "zod";
 import { tagRead } from "./tagRead.ts";
 
-export const TagTypeSchema = z.string().or(z.array(z.string()));
+export const TagTypeSchema = z.array(z.string());
 export type TagType = z.infer<typeof TagTypeSchema>;
 
 export const tagSave = async (tags: TagType): Promise<void> => {
   try {
     const readtag = await tagRead();
 
-    for (const result of readtag) {
-      for (const tag of tags) {
-        if (!result.includes(tag)) {
-          readtag.push(tag);
-        }
+    for (const tag of tags) {
+      if (!readtag.includes(tag)) {
+        readtag.push(tag);
       }
     }
 
     await mkdir(ROOT_DIR, { recursive: true });
+
     const tagsJson = JSON.stringify(readtag, null, 2);
+
     await writeFile(`${ROOT_DIR}/tags.json`, tagsJson, "utf-8");
   } catch (error) {
     consola.error(error);
-    return;
   }
 };
