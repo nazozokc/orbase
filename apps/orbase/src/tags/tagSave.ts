b66af827342a3bfb795c35b52be249dec9113363
nazozokc <nazozokc@users.noreply.github.com>
2026-09-06 23:12:21 +0900

    edit

--- apps/orbase/src/tags/tagSave.ts
+++ apps/orbase/src/tags/tagSave.ts
@@ -2,21 +2,25 @@ import { writeFile, mkdir } from "node:fs/promises";
 import { ROOT_DIR } from "../constant/app.ts";
 import consola from "consola";
 import z from "zod";
+import { tagRead } from "./tagRead.ts";
 
 export const TagTypeSchema = z.string().or(z.array(z.string()));
 export type TagType = string[];
 
 export const tagSave = async (tags: TagType): Promise<void> => {
   try {
-    const result = TagTypeSchema.safeParse(tags);
+    const readtag = await tagRead();
 
-    if (!result.success) {
-      consola.error(result);
-      return;
+    for (const result of readtag) {
+      for (const tag of tags) {
+        if (!result.includes(tag)) {
+          readtag.push(tag);
+        }
+      }
     }
 
     await mkdir(ROOT_DIR, { recursive: true });
-    const tagsJson = JSON.stringify(result, null, 2);
+    const tagsJson = JSON.stringify(readtag, null, 2);
     await writeFile(`${ROOT_DIR}/tags.json`, tagsJson, "utf-8");
   } catch (error) {
     consola.error(error);
