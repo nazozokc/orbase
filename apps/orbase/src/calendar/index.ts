05ae953d8c21821d2c4b31a50618cfefcefe103c
nazozokc <nazozokc@users.noreply.github.com>
2026-09-10 22:32:23 +0900

    edit

--- apps/orbase/src/calendar/index.ts
+++ apps/orbase/src/calendar/index.ts
@@ -2,48 +2,52 @@ import Table from "cli-table3";
 import { consola } from "consola";
 
 export const calendar = (year?: number, month?: number): void => {
-  const now = new Date();
-  const years = year ?? now.getFullYear();
-  const months = month !== undefined ? month - 1 : now.getMonth();
+  try {
+    const now = new Date();
+    const years = year ?? now.getFullYear();
+    const months = month !== undefined ? month - 1 : now.getMonth();
 
-  const firstDay = new Date(years, months, 1).getDay();
-  const dateInMonth = new Date(years, months, 0).getDate();
+    const firstDay = new Date(years, months, 1).getDay();
+    const dateInMonth = new Date(years, months, 0).getDate();
 
-  const table = new Table({
-    head: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fry", "Sat"],
-  });
+    const table = new Table({
+      head: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fry", "Sat"],
+    });
 
-  let row: string[] = [];
+    let row: string[] = [];
 
-  for (let i = 0; i < firstDay; i++) {
-    row.push("");
-  }
-
-  for (let day = 1; day < dateInMonth; day++) {
-    if (
-      years === now.getFullYear() &&
-      months === now.getMonth() &&
-      day === now.getDate()
-    ) {
-      row.push(`[${String(day)}]`);
-    }
-
-    if (day !== now.getDate()) {
-      row.push(String(day));
-    }
-
-    if (row.length === 7) {
-      table.push(row);
-      row = [];
-    }
-  }
-
-  if (row.length > 0) {
-    while (row.length < 7) {
+    for (let i = 0; i < firstDay; i++) {
       row.push("");
     }
-    table.push(row);
-  }
 
-  consola.log(table.toString());
+    for (let day = 1; day < dateInMonth; day++) {
+      if (
+        years === now.getFullYear() &&
+        months === now.getMonth() &&
+        day === now.getDate()
+      ) {
+        row.push(`[${String(day)}]`);
+      }
+
+      if (day !== now.getDate()) {
+        row.push(String(day));
+      }
+
+      if (row.length === 7) {
+        table.push(row);
+        row = [];
+      }
+    }
+
+    if (row.length > 0) {
+      while (row.length < 7) {
+        row.push("");
+      }
+      table.push(row);
+    }
+
+    consola.log(table.toString());
+  } catch (error) {
+    consola.error(error);
+  }
 };
