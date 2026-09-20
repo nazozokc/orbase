1a27a9accf0101a75d470b18938faa29a38663ae
nazozokc <nazozokc@users.noreply.github.com>
2026-09-20 23:38:54 +0900

    edit

--- apps/orbase/src/config/generate.ts
+++ apps/orbase/src/config/generate.ts
@@ -10,7 +10,6 @@ export const generateConfig = async (): Promise<void> => {
     save_directory: join(`${homedir()}`, ".orbase"),
   };
 
-  await mkdir(CONFIG_DIR, { recursive: true });
   const stringify = JSON.stringify(defaultSchema, null, 2);
   await writeFile(CONFIG_DIR_NAME, stringify, "utf-8");
 };
